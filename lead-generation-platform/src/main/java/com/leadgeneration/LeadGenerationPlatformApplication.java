72ead449328481f3a9a4da60946b818ac73a9c70
Gopikumar12345 <gopikumarkishan15042001@gmail.com>
2026-06-22 18:20:13 +0530

    Added Spring Boot backend project structure

---
+++ lead-generation-platform/src/main/java/com/leadgeneration/LeadGenerationPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.leadgeneration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeadGenerationPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeadGenerationPlatformApplication.class, args);
+	}
+
+}
